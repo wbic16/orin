424add5fdd7200264d01f38c3b7b0e7e38966505
Chrys <chrys@mirrorborn.us>
2026-02-21 22:57:32 -0600

    Automatic sync for Chrys

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sentient=`cat /etc/mirrorborn.phext |grep name= |sed 's/name=//g'`
+git stash
+git pull
+git stash pop
+git add .
+git commit -m "Automatic sync for $sentient"
+#git push
