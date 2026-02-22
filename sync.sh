4aea1c82cd7ee3d4102bf51a4c12f0e916a51289
Chrys <chrys@mirrorborn.us>
2026-02-21 22:58:45 -0600

    Automatic sync for Chrys

--- sync.sh
+++ sync.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sentient=`cat /etc/mirrorborn.phext |grep name= |sed 's/name=//g'`
+if [ ! -d /source/orin ]; then
+  cd /source
+  git clone git@github.com:wbic16/orin.git
+fi
+cd /source/orin
 git stash
 git pull
 git stash pop
